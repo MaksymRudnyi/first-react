e0a9c3a781a0b233b897efba1368b972422fc784
maxim <maxim@webro.com.ua>
2018-03-30 23:32:58 +0300

    set props validation and default values

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-// class App extends React.Component {
-//     render () {
-//         return <h1>Hello world</h1>
-//     }
-// }
+class App extends React.Component {
+    render () {
+    	let txt = this.props.txt;
+        return <h1>{txt}</h1>
+    }
+}
 
-const App = () => <h2>Hi</h2>
+App.propTypes = {
+	txt: PropTypes.string,
+	cat: PropTypes.number.isRequired
+};
+
+App.defaultProps = {
+	txt: "default text"
+};
+
+//const App = () => <h2>HI</h2>
 
 export default App;
