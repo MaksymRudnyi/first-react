00ed1982b24bba8a55c933df8871b61ca68d55c8
maxim <maxim@webro.com.ua>
2018-03-31 15:20:14 +0300

    put nested component inside dump component which render it. use props.children

--- src/App.js
+++ src/App.js
@@ -1,29 +1,17 @@
 import React from 'react';
 
 class App extends React.Component {
-	constructor () {
-		super();
-		this.state = {
-			txt: 'this is default state',
-			cat: 0
-		}
-	}
-
-	update (e) {
-		this.setState({txt: e.target.value})
-	}
-
     render () {
-        return (
-			<div>
-				<h1>{this.state.txt} - {this.state.cat}</h1>
-				<Widget update={this.update.bind(this)} />
-			</div>
-		)
+        return <Button>I <Heart/> React</Button>
     }
 }
 
-const Widget = (props) =>
-	<input type="text" onChange={props.update} />
+const Button = (props) => <button>{props.children}</button>
+
+class Heart extends React.Component {
+	render () {
+		return <span>&hearts;</span>
+	}
+}
 
 export default App;
